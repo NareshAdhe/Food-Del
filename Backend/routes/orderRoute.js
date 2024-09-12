d5f7e325b748d63db752f0eab26824576fa668a5
Naresh Adhe <nareshadhe01@gmail.com>
2024-09-12 14:14:59 +0530

    Made Order Verification Changes

--- Backend/routes/orderRoute.js
+++ Backend/routes/orderRoute.js
@@ -1,11 +1,12 @@
 import express from "express"
 import {placeOrder,verifyOrder,userOrders, listOrders,updateStatus} from "../controllers/orderController.js"
 import {authMiddleware} from "../middlewares/authMiddleware.js"
+const frontend_url = "https://food-del-e9ds.onrender.com";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/place",authMiddleware,placeOrder);
-orderRouter.post("/verify",verifyOrder);
+orderRouter.post(`${frontend_url}/verify`,verifyOrder);
 orderRouter.post("/userorders",authMiddleware,userOrders);
 orderRouter.get("/list",listOrders);
 orderRouter.post("/status",updateStatus);
