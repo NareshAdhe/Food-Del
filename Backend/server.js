18ed6c46207832fbf99d4bac681a8a9ba3a25e7c
Naresh Adhe <nareshadhe01@gmail.com>
2024-09-13 08:18:20 +0530

    Added Middleware For Reloading Problem

--- Backend/server.js
+++ Backend/server.js
@@ -14,6 +14,9 @@ const port = process.env.PORT || 4000;
 //middleware
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 
 //DB Connection
 connectDB();
